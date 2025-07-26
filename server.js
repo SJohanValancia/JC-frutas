f12a30c1d13ab232cc7a025d4691dfa18ff6da09
Johan Valencia <johanmejiaxlarchaval@gmail.com>
2025-07-26 15:48:54 -0500

    render activado 2

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({ origin: "http://127.0.0.1:5500", credentials: true }));
+app.use(cors({
+  origin: ["http://127.0.0.1:5500", "https://jc-frutas.netlify.app"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
