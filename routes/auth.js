7ea3b4f58ac0a380f9aedf15209b861b51cc4d0e
Johan Valencia <johanmejiaxlarchaval@gmail.com>
2025-08-26 15:02:51 -0500

    dos admins 3

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./models/User");
+const User = require('../models/User')
+
 
 
 router.post("/register", async (req, res) => {
