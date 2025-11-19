096726acfc2ce21e3201e0fe94c0a3b508e62db4
Johan Steven Valencia Mejias <johanvalencia@MacBook-Air-de-Johan.local>
2025-11-19 11:59:12 -0500

    cors

--- www/server.js
+++ www/server.js
@@ -71,6 +71,7 @@ app.use(cors({
       'https://jc-fi.netlify.app',
       'https://jc-fi.onrender.com',
       'https://jc-frutas.onrender.com',
+      'https://jc-frutas.netlify.app',
       'http://localhost:5000',
       'http://127.0.0.1:5000',
       'http://localhost:3000',
